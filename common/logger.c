225cd92b4849bafa3017e72f8b64fbde7c70d462
Philipp Hochmann <phil.hochmann@gmail.com>
2021-03-15 00:52:31 +0100

    Add 4B5B encoding

---
+++ common/logger.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <avr/io.h>
+#include "logger.h"
+
+#define MAX_PRINTF_LENGTH 30
+
+void logger_init()
+{
+    // Baud rate: 500000 bps
+    UBRR0H = (uint8_t)(3 >> 8);
+    UBRR0L = (uint8_t)(3 & 0xFF);
+    UCSR0A = (1 << U2X0);                  // Double speed
+    UCSR0B = (0 << RXEN0) | (1 << TXEN0);  // disable the receiver, enable transmitter
+    UCSR0C = (1 << USBS0) | (3 << UCSZ00); // 2 stop bits, 8 bit data
+}
+
+void logger_print(const char *str)
+{
+    while (*str != '\0')
+    {
+        while (!(UCSR0A & (1 << UDRE0)));
+        UDR0 = *str;
+        str++;      
+    }
+}
+
+void logger_printf(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    char str[MAX_PRINTF_LENGTH];
+    vsnprintf(str, MAX_PRINTF_LENGTH, fmt, args);
+    logger_print(str);
+}
---
+++ transmitter/transmitter.c
@@ -0,0 +1,52 @@
+#define F_CPU 16000000L
+#include <stdbool.h>
+#include <stdint.h>
+#include <stddef.h>
+
+#include <util/delay.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#define RADIO_TX
+#include "../common/radio.h"
+#include "../common/logger.h"
+
+// ADC0 = Port C, Pin 0 ANALOG
+// ADC1 = Port C, Pin 1 ANALOG
+
+void adc_init()
+{
+    // Prescaler 128
+    ADCSRA = (1 << ADEN) | (0 << ADATE) | (0 << ADIE) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+    ADMUX = (0 << REFS1) | (1 << REFS0) | (1 << ADLAR);
+    DDRC &= (0 << 0) | (0 << 1);
+    PORTC |= (1 << 0) | (1 << 1);
+}
+
+uint8_t get_adc_value(uint8_t mux)
+{
+    ADMUX &= 0b11110000;
+    ADMUX |= (mux & 0b00001111);
+    ADCSRA |= (1 << ADSC); // Start conversion
+    while ((ADCSRA & (1 << ADIF)) == 0); // Wait until conversion has finished
+    ADCSRA &= ~(1 << ADIF); // Clear flag
+    return ADCH;
+}
+
+int main()
+{
+    adc_init();
+    radio_init_tx();
+    logger_init();
+
+    logger_print("Transmitter started!\n");
+
+    while (true)
+    {
+        uint8_t to_send[2] = { get_adc_value(0), get_adc_value(1) };
+        logger_printf("%d %d\n", to_send[0], to_send[1]);
+        radio_send(to_send, 2);
+    }
+
+    return 0;
+}
